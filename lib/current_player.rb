4e33802122053f7cf4fbd690f7a70ba2a50311dd
Christopher Finch <christopher.h.finch@gmail.com>
2018-02-06 05:44:42 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+#def turn_count(board)
+  #board = ["X", "Y", " "]
+#  counter = 0
+#  board.each do |moves|
+#  if moves== "O" || moves== "X"
+#    counter += 1
+    #puts "#{counter}
+#    end
+#print "#{counter}"
+#end
+#end
+
+def turn_count(board)
+  repeat = board.size
+  counter = 0
+  moved = 0
+  while counter < repeat
+    if board[counter] == "O" || board[counter] == "X"
+      moved += 1
+      puts "#{moved}"
+    end
+    counter += 1
+  end
+end
